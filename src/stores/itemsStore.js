cfdaef659a91220105d11fd65382a57aebce69e9
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-05-28 11:12:45 +0530

    Adding code

--- src/stores/itemsStore.js
+++ src/stores/itemsStore.js
@@ -1,64 +1,68 @@
 import { create } from "zustand";
 import { initialItems } from "../lib/init";
+import { persist } from "zustand/middleware";
 
-export const useItemsStore = create((set) => ({
-  items: initialItems,
+export const useItemsStore = create(
+  persist((set) => ({
+    items: initialItems,
 
-  addItem: (value) => {
-    const newItem = {
-      name: value,
-      packed: false,
-      id: new Date().getTime(),
-    };
+    addItem: (value) => {
+      const newItem = {
+        name: value,
+        packed: false,
+        id: new Date().getTime(),
+      };
 
-    set((state) => ({ items: [...state.items, newItem] }));
-  },
+      set((state) => ({ items: [...state.items, newItem] }));
+    },
 
-  deleteItem: (id) => {
-    set((state) => {
-      const NewItems = state.items.filter((item) => item.id !== id);
-      return({items : NewItems});
-    });
-  },
-
-  toggleItem : (id) => {
-    set((state) => {
-    const NewItems = state.items.map(item => {
-      if (item.id === id) {
-        return { ...item, packed: !item.packed };
-      }
-      return item;
-    });
-    return({items : NewItems});
-    });
-  },
-
-  removeAllItems: () => {
-    set(() => ({ items: [] }));
-  },
-
-  resetToInitial: () => {
-    set(() => ({ items: initialItems }));
-  },
-
-  markAllAsComplete: () => {
-    set((state) => {
-      const newItems = state.items.map((item) => {
-        return { ...item, packed: true };
+    deleteItem: (id) => {
+      set((state) => {
+        const NewItems = state.items.filter((item) => item.id !== id);
+        return { items: NewItems };
       });
+    },
 
-      return { items: newItems };
-    });
-  },
-
-  markAllAsIncomplete: () => {
-    set((state) => {
-      const NewItems = state.items.map((item) => {
-        return { ...item, packed: false };
+    toggleItem: (id) => {
+      set((state) => {
+        const NewItems = state.items.map((item) => {
+          if (item.id === id) {
+            return { ...item, packed: !item.packed };
+          }
+          return item;
+        });
+        return { items: NewItems };
       });
+    },
 
-      return { items: NewItems };
-    });
-  },
+    removeAllItems: () => {
+      set(() => ({ items: [] }));
+    },
 
-}));
+    resetToInitial: () => {
+      set(() => ({ items: initialItems }));
+    },
+
+    markAllAsComplete: () => {
+      set((state) => {
+        const newItems = state.items.map((item) => {
+          return { ...item, packed: true };
+        });
+
+        return { items: newItems };
+      });
+    },
+
+    markAllAsIncomplete: () => {
+      set((state) => {
+        const NewItems = state.items.map((item) => {
+          return { ...item, packed: false };
+        });
+
+        return { items: NewItems };
+      });
+    },
+  }) , {
+    name : "items"
+  })
+);
